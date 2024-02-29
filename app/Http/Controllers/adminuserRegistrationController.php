3c8f20653fda78e5142555dadf21d89b6f60074e
AnitaSeth1 <anita.glansa@gmail.com>
2024-02-29 12:54:40 +0530

    inserting restration form

--- app/Http/Controllers/adminuserRegistrationController.php
+++ app/Http/Controllers/adminuserRegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Registration;
 
 class adminuserRegistrationController extends Controller
 {
@@ -14,16 +15,28 @@ class adminuserRegistrationController extends Controller
     }
 
     public function view(Request $request)
-    {
-        $request->validate(
-            [
-                'username' => 'required',
-                'email' => 'required|email',
-                'password' => 'required'
-            ]
-        );
+{
+    $request->validate([
+        'username' => 'required',
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
 
-        echo "<pre>";
-        print_r($request->all());
-    }
+    // Create a new instance of the Registration model
+    $registration = new Registration();
+
+    // Assign values to the model properties
+    $registration->name = $request->input('username');
+    $registration->email = $request->input('email');
+    $registration->password = $request->input('password');
+
+    // Save the model to the database
+    $registration->save();
+
+    // You might want to redirect or return a response here
+    // For example:
+    // return redirect()->back()->with('success', 'Registration successful!');
+
+    return redirect('/')->with('success', 'Registration successful! You can now login.');
+}
 }
--- resources/views/security/registration.blade.php
+++ resources/views/security/registration.blade.php
@@ -29,6 +29,7 @@
 
                     <form id="formAuthentication" class="mb-3" action="{{ url('/') }}/registration" method="POST">
                         @csrf
+                        
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
                             <input type="text" class="form-control" id="username" name="username"
