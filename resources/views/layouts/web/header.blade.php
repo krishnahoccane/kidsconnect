3f84d5429f24482e864279e89bc06e04be6bf928
AnitaSeth1 <anita.glansa@gmail.com>
2024-03-05 10:32:44 +0530

    dashboard

--- resources/views/layouts/web/header.blade.php
+++ resources/views/layouts/web/header.blade.php
@@ -106,7 +106,7 @@
                                     fill="#7367F0" />
                             </svg>
                         </span>
-                        <span class="app-brand-text demo menu-text fw-bold">Kids connect</span>
+                        <span class="app-brand-text demo menu-text fw-bold">Kids Connect</span>
                     </a>
 
                     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
