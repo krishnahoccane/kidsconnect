7ab237e121bae3f7ec0f751ddd4e7333f8832ade
AnitaSeth1 <anita.glansa@gmail.com>
2024-03-11 16:35:29 +0530

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,14 +5,15 @@
             <div class="card-body">
                 <div class="d-flex align-items-center mb-2 pb-1">
                     <div class="avatar me-2">
-                        <span class="avatar-initial rounded bg-label-primary"><i class="ti ti-truck ti-md"></i></span>
+                        <span class="avatar-initial rounded bg-label-primary"><i class="fa fa-users" aria-hidden="true"></i>
+                        </span>
                     </div>
                     <h4 class="ms-1 mb-0">42</h4>
                 </div>
                 <p class="mb-1">Subscriber</p>
                 <p class="mb-0">
-                    <span class="fw-medium me-1">+18.2%</span>
-                    <small class="text-muted">than last week</small>
+                    {{-- <span class="fw-medium me-1">+18.2%</span>
+                    <small class="text-muted">than last week</small> --}}
                 </p>
             </div>
         </div>
@@ -22,15 +23,14 @@
             <div class="card-body">
                 <div class="d-flex align-items-center mb-2 pb-1">
                     <div class="avatar me-2">
-                        <span class="avatar-initial rounded bg-label-warning"><i
-                                class='ti ti-alert-triangle ti-md'></i></span>
+                        <span class="avatar-initial rounded bg-label-warning"><i class="fa-solid fa-user"></i></span>
                     </div>
                     <h4 class="ms-1 mb-0">8</h4>
                 </div>
-                <p class="mb-1">Vehicles with errors</p>
+                <p class="mb-1">Admin</p>
                 <p class="mb-0">
-                    <span class="fw-medium me-1">-8.7%</span>
-                    <small class="text-muted">than last week</small>
+                    {{-- <span class="fw-medium me-1">-8.7%</span>
+                    <small class="text-muted">than last week</small> --}}
                 </p>
             </div>
         </div>
@@ -40,14 +40,14 @@
             <div class="card-body">
                 <div class="d-flex align-items-center mb-2 pb-1">
                     <div class="avatar me-2">
-                        <span class="avatar-initial rounded bg-label-danger"><i class='ti ti-git-fork ti-md'></i></span>
+                        <span class="avatar-initial rounded bg-label-danger"><i class="fa-solid fa-child"></i></span>
                     </div>
                     <h4 class="ms-1 mb-0">27</h4>
                 </div>
-                <p class="mb-1">Deviated from route</p>
+                <p class="mb-1">Kids</p>
                 <p class="mb-0">
-                    <span class="fw-medium me-1">+4.3%</span>
-                    <small class="text-muted">than last week</small>
+                    {{-- <span class="fw-medium me-1">+4.3%</span>
+                    <small class="text-muted">than last week</small> --}}
                 </p>
             </div>
         </div>
@@ -57,14 +57,88 @@
             <div class="card-body">
                 <div class="d-flex align-items-center mb-2 pb-1">
                     <div class="avatar me-2">
-                        <span class="avatar-initial rounded bg-label-info"><i class='ti ti-clock ti-md'></i></span>
+                        <span class="avatar-initial rounded bg-label-info"><i class="fa-solid fa-hand"></i></span>
                     </div>
                     <h4 class="ms-1 mb-0">13</h4>
                 </div>
-                <p class="mb-1">Late vehicles</p>
+                <p class="mb-1">Request</p>
                 <p class="mb-0">
-                    <span class="fw-medium me-1">-2.5%</span>
-                    <small class="text-muted">than last week</small>
+                    {{-- <span class="fw-medium me-1">-2.5%</span>
+                    <small class="text-muted">than last week</small> --}}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6 col-lg-3 mb-4">
+        <div class="card card-border-shadow-primary">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-2 pb-1">
+                    <div class="avatar me-2">
+                        <span class="avatar-initial rounded bg-label-primary"><i class="fa fa-check" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <h4 class="ms-1 mb-0">42</h4>
+                </div>
+                <p class="mb-1">Requested Approvals</p>
+                <p class="mb-0">
+                    {{-- <span class="fw-medium me-1">+18.2%</span>
+                    <small class="text-muted">than last week</small> --}}
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 mb-4">
+        <div class="card card-border-shadow-warning">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-2 pb-1">
+                    <div class="avatar me-2">
+                        <span class="avatar-initial rounded bg-label-warning"><i class="fas fa-question-circle" style="font-size: 24px; color: orange;"></i>
+
+                        </span>
+                    </div>
+                    <h4 class="ms-1 mb-0">8</h4>
+                </div>
+                <p class="mb-1">Requested Pendings</p>
+                <p class="mb-0">
+                    {{-- <span class="fw-medium me-1">-8.7%</span>
+                    <small class="text-muted">than last week</small> --}}
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 mb-4">
+        <div class="card card-border-shadow-danger">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-2 pb-1">
+                    <div class="avatar me-2">
+                        <span class="avatar-initial rounded bg-label-danger"><i class="fas fa-times-circle" style="font-size: 24px; color: red;"></i>
+                        </span>
+                    </div>
+                    <h4 class="ms-1 mb-0">27</h4>
+                </div>
+                <p class="mb-1">Requested Rejectes</p>
+                <p class="mb-0">
+                    {{-- <span class="fw-medium me-1">+4.3%</span>
+                    <small class="text-muted">than last week</small> --}}
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 mb-4">
+        <div class="card card-border-shadow-info">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-2 pb-1">
+                    <div class="avatar me-2">
+                        <span class="avatar-initial rounded bg-label-info"><i class="fa fa-times" aria-hidden="true"></i></span>
+                    </div>
+                    <h4 class="ms-1 mb-0">13</h4>
+                </div>
+                <p class="mb-1">Requested Cancelleds</p>
+                <p class="mb-0">
+                    {{-- <span class="fw-medium me-1">-2.5%</span>
+                    <small class="text-muted">than last week</small> --}}
                 </p>
             </div>
         </div>
