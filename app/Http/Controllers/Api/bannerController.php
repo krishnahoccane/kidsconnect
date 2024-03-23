c375780e20ba38ef48e85839db682d0a7195b9fd
Glansa-Solutions <glansa.solutions777@gmail.com>
2024-03-23 10:57:36 +0530

    Comments added for understanding to Banner Store function

--- app/Http/Controllers/Api/bannerController.php
+++ app/Http/Controllers/Api/bannerController.php
@@ -8,35 +8,6 @@ use Illuminate\Http\Request;
 
 class bannerController extends Controller
 {
-    // public function store(Request $request)
-    // {
-    //     // Validate request data
-    //     // $request->validate([
-    //     //     'name' => 'required',
-    //     // ]);
-
-    //     // Check if a file is present in the request
-    //     if ($request->hasFile('image')) {
-    //         // Upload image file to the storage directory
-    //         $imagePath = $request->file('file')->store('public/ui/assets/img/backgrounds/');
-
-    //         // Create new banner record
-    //         $banner = new Banner();
-    //         $banner->image = $imagePath;
-    //         $banner->save();
-
-    //         return response()->json([
-    //             'status' => 200,
-    //             'message' => 'Banner inserted successfully',
-    //             'data' => $banner
-    //         ], 200);
-    //     } else {
-    //         return response()->json([
-    //             'status' => 400,
-    //             'message' => 'No image file uploaded'
-    //         ], 400);
-    //     }
-    // }
     public function index()
     {
         $banners = Banner::all();
@@ -56,27 +27,26 @@ class bannerController extends Controller
 
     public function store(Request $request)
     {
-
-
-         $request->validate([
+        // Here we are taking the images array and validating them
+        $request->validate([
             'images.*' => 'required|image|mimes:jpg,jpeg,png,webp',
         ]);
-        $imageData = [];
+        $imageData = [];//declaring an empty array to store  the Images 
         if ($files = $request->file('images')) {
-            $now = now();
+            // Looping the file and storing in the folder
             foreach ($files as $file) {
-                
                 $extension = $file->getClientOriginalExtension();
                 $fileName = time() . '_' . uniqid() . '.' . $extension;
 
                 $path = "uploads/banners/";
                 $file->move($path, $fileName);
-
+                //gving the column data in array of imageData
                 $imageData[] = [
                     'image' => $path . $fileName,
-                    'created_at'=>now(),
+                    'created_at' => now(),
                 ];
-            } 
+            }
+
         }
         $banners = Banner::insert($imageData);
         if ($banners) {
