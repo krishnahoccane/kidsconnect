8f81c7788430ac3a5bcd59e8fd50f60a0e1d5afd
Glansa-Solutions <glansa.solutions777@gmail.com>
2024-04-10 15:39:22 +0530

    catched up the name of subscriber

--- app/Http/Controllers/Api/subscriberController.php
+++ app/Http/Controllers/Api/subscriberController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\subscriberlogins;
 use App\Models\subscribersModel;
 use App\Http\Controllers\Controller;
 
@@ -15,12 +16,19 @@ class subscriberController extends Controller
 
     public function index()
     {
-        //
-        $subscriber = subscribersModel::all();
-        if ($subscriber->count() > 0) {
+        // comment - vishal - Here pluk is used to get perticular column of table and toArray() used to fetch in array form,
+        // After fetching the id from subscriber table passing that into the subscriberlogin table and fetching sepecfic condition
+        // MainsubscriberId is Subscriber table id & IsMain 1 - Selected fields are Id, FName, LName, Email, RID, CreatedDate
+
+        $subscriberIds = subscribersModel::pluck('id')->toArray();
+        $subscriberLoginData = subscriberlogins::whereIn('MainSubscriberId', $subscriberIds)->where('IsMain', 1)
+            ->select('id', 'FirstName', 'LastName', 'Email', 'RoleId', 'created_at')
+            ->get();
+
+        if ($subscriberLoginData->count() > 0) {
             return response()->json([
                 'status' => 200,
-                'data' => $subscriber
+                'data' => $subscriberLoginData
             ], 200);
         } else {
             return response()->json([
@@ -28,6 +36,7 @@ class subscriberController extends Controller
                 'message' => 'No Data Found'
             ], 404);
         }
+
     }
 
     /**
